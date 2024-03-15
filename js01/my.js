1c46286b0aa8fe334e9fd7a9ed4bf3779765ea83
Jacobyou19 <jacobyou19@gmail.com>
2024-03-15 11:16:15 +0800

    AM

---
+++ js01/my.js
@@ -0,0 +1,37 @@
+console.log("8" + "2");
+console.log(888);
+console.log(6+4+"5");
+console.log("5"+6+4);
+console.log(6 + 4);
+
+console.log(true);
+console.log(false);
+console.log(6>5);
+console.log(6>=5);
+console.log(6 < 5);
+
+let a;
+a = 5;
+a = 10;
+console.log(a);
+
+let x = ["volvo", "BMW", "TOYOTA"];
+console.log (typeof x);
+console.log (x);
+
+let y = { type: "volvo", model: "500", color: "white" };
+console.log(typeof y)
+console.log(y)
+
+
+let S = document.getElementById("tt1");
+tt1.innerHTML = "Hi boy"; 
+
+const d = 100;
+console.log(d);
+
+document.write("HELLO");
+document.getElementById("tt").innerHTML = "Welcome"
+document.getElementById("tt1").innerHTML = "Good day"
+
+
