2af0e8b42211344c2b19bd10a7069cffc37a6f98
Jacobyou19 <jacobyou19@gmail.com>
2024-03-15 14:28:37 +0800

    AM

--- 上課資料/郭嘉雯/07-函式function/07-1-function寫法/my.js
+++ 上課資料/郭嘉雯/07-函式function/07-1-function寫法/my.js
@@ -1,4 +1,8 @@
 
 function aaa() {
-    document.getElementById("pic").src = "./img/kiwi2.jpg"
+    let PIC = document.getElementById("pic");
+    PIC.src = "./img/kiwi2.jpg"
 }
+
+
+setTimeout(aaa,5000)
