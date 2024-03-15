f9f86c930d5356e0b6e5aa35dca826a65e7a9724
Jacobyou19 <jacobyou19@gmail.com>
2024-03-15 16:55:46 +0800

    am

---
+++ 上課資料/郭嘉雯 - 原始範例/12-迴圈-for/12-4-九宮格圖片-用for/my.js
@@ -0,0 +1,52 @@
+
+document.getElementById("pic1").onmouseover = aaa;
+document.getElementById("pic2").onmouseover = aaa;
+document.getElementById("pic3").onmouseover = aaa;
+document.getElementById("pic4").onmouseover = aaa;
+document.getElementById("pic5").onmouseover = aaa;
+document.getElementById("pic6").onmouseover = aaa;
+document.getElementById("pic7").onmouseover = aaa;
+document.getElementById("pic8").onmouseover = aaa;
+document.getElementById("pic9").onmouseover = aaa;
+
+document.getElementById("pic1").onmouseout = bbb;
+document.getElementById("pic2").onmouseout = bbb;
+document.getElementById("pic3").onmouseout = bbb;
+document.getElementById("pic4").onmouseout = bbb;
+document.getElementById("pic5").onmouseout = bbb;
+document.getElementById("pic6").onmouseout = bbb;
+document.getElementById("pic7").onmouseout = bbb;
+document.getElementById("pic8").onmouseout = bbb;
+document.getElementById("pic9").onmouseout = bbb;
+
+function aaa() {
+
+    if (this.id == "pic1") { this.src = "./img/ice01-1.jpg" }
+    if (this.id == "pic2") { this.src = "./img/ice02-1.jpg" }
+    if (this.id == "pic3") { this.src = "./img/ice03-1.jpg" }
+    if (this.id == "pic4") { this.src = "./img/ice04-1.jpg" }
+    if (this.id == "pic5") { this.src = "./img/ice05-1.jpg" }
+    if (this.id == "pic6") { this.src = "./img/ice06-1.jpg" }
+    if (this.id == "pic7") { this.src = "./img/ice07-1.jpg" }
+    if (this.id == "pic8") { this.src = "./img/ice08-1.jpg" }
+    if (this.id == "pic9") { this.src = "./img/ice09-1.jpg" }
+
+}
+
+
+function bbb() {
+
+    if (this.id == "pic1") { this.src = "./img/ice01.jpg" }
+    if (this.id == "pic2") { this.src = "./img/ice02.jpg" }
+    if (this.id == "pic3") { this.src = "./img/ice03.jpg" }
+    if (this.id == "pic4") { this.src = "./img/ice04.jpg" }
+    if (this.id == "pic5") { this.src = "./img/ice05.jpg" }
+    if (this.id == "pic6") { this.src = "./img/ice06.jpg" }
+    if (this.id == "pic7") { this.src = "./img/ice07.jpg" }
+    if (this.id == "pic8") { this.src = "./img/ice08.jpg" }
+    if (this.id == "pic9") { this.src = "./img/ice09.jpg" }
+
+}
+
+
+
--- 上課資料/郭嘉雯/08-運算符號/08-2-設定選購商品數量/my.js
+++ 上課資料/郭嘉雯/08-運算符號/08-2-設定選購商品數量/my.js
@@ -1,3 +1,15 @@
+let x = 12
+document.getElementById("add").onclick = aaa
+document.getElementById("minus").onclick = bbb
 
-
+function aaa() { 
+    x=x+1
+    document.getElementById("num").innerHTML=x
+}
+function bbb() {
+    x = x - 1
+    document.getElementById("num").innerHTML = x
+    x == 0
+    x = x+0
+} 
 
