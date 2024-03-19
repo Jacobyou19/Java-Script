4a70cb66d86588df351ed8ecfb18d42f5bffe3c0
Jacobyou19 <jacobyou19@gmail.com>
2024-03-19 11:51:50 +0800

    AM

--- 上課資料/郭嘉雯/09-滑鼠事件-event/09-2-滑鼠滑入-滑出-按下-放開/my.js
+++ 上課資料/郭嘉雯/09-滑鼠事件-event/09-2-滑鼠滑入-滑出-按下-放開/my.js
@@ -1,19 +1,30 @@
+let PIC = document.getElementById("pic")
+let N = 0
+let audio = document.createElement("audio")
 document.getElementById("pic").onmouseover = aaa;
-document.getElementById("pic").onmouseover = bbb;
+document.getElementById("pic").onmouseout = bbb;
 document.getElementById("pic").onmousedown = ccc;
 document.getElementById("pic").onmouseup = aaa;
 
-function aaa() { 
+function aaa() {
 
     this.src = "./img/p2.png"
 }
 
 function bbb() {
-    
+
     this.src = "./img/p1.png"
 }
 
 function ccc() {
-
+    N = N + 1
     this.src = "./img/p3.png"
+    document.getElementById("tt").innerHTML = N
+    audioplay()
+}
+
+function autoplay() {
+    audio.src = "./a.mp3";
+    audio.play
+
 }
---
+++ 上課資料/郭嘉雯/10-物件本身-this/10-2-圖片遞增/my.js
@@ -0,0 +1,9 @@
+let N = 1; 
+
+let CAKE= document.getElementById("cake").onclick = aaa;
+
+function aaa() {
+    N += 1
+   this.src = "./img/photo" +N+ ".jpg"
+
+}
