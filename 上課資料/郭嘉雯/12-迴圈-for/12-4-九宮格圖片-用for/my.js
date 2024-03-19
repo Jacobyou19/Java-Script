096a5f6cacf0a4995a6f2d5fcbf3ea8c4b250ee0
Jacobyou19 <jacobyou19@gmail.com>
2024-03-19 16:58:20 +0800

    AM

--- 上課資料/郭嘉雯/12-迴圈-for/12-4-九宮格圖片-用for/my.js
+++ 上課資料/郭嘉雯/12-迴圈-for/12-4-九宮格圖片-用for/my.js
@@ -1,52 +1,57 @@
-
-document.getElementById("pic1").onmouseover = aaa;
-document.getElementById("pic2").onmouseover = aaa;
-document.getElementById("pic3").onmouseover = aaa;
-document.getElementById("pic4").onmouseover = aaa;
-document.getElementById("pic5").onmouseover = aaa;
-document.getElementById("pic6").onmouseover = aaa;
-document.getElementById("pic7").onmouseover = aaa;
-document.getElementById("pic8").onmouseover = aaa;
-document.getElementById("pic9").onmouseover = aaa;
-
-document.getElementById("pic1").onmouseout = bbb;
-document.getElementById("pic2").onmouseout = bbb;
-document.getElementById("pic3").onmouseout = bbb;
-document.getElementById("pic4").onmouseout = bbb;
-document.getElementById("pic5").onmouseout = bbb;
-document.getElementById("pic6").onmouseout = bbb;
-document.getElementById("pic7").onmouseout = bbb;
-document.getElementById("pic8").onmouseout = bbb;
-document.getElementById("pic9").onmouseout = bbb;
+for (let i = 1; i <= 9;i++){
+    document.getElementById("pic"+i).onmouseover = aaa;
+}
+// document.getElementById("pic1").onmouseover = aaa;
+// document.getElementById("pic2").onmouseover = aaa;
+// document.getElementById("pic3").onmouseover = aaa;
+// document.getElementById("pic4").onmouseover = aaa;
+// document.getElementById("pic5").onmouseover = aaa;
+// document.getElementById("pic6").onmouseover = aaa;
+// document.getElementById("pic7").onmouseover = aaa;
+// document.getElementById("pic8").onmouseover = aaa;
+// document.getElementById("pic9").onmouseover = aaa;
+for (let i = 1; i <= 9; i++) {
+    document.getElementById("pic" + i).onmouseout = bbb;
+}
+// document.getElementById("pic1").onmouseout = bbb;
+// document.getElementById("pic2").onmouseout = bbb;
+// document.getElementById("pic3").onmouseout = bbb;
+// document.getElementById("pic4").onmouseout = bbb;
+// document.getElementById("pic5").onmouseout = bbb;
+// document.getElementById("pic6").onmouseout = bbb;
+// document.getElementById("pic7").onmouseout = bbb;
+// document.getElementById("pic8").onmouseout = bbb;
+// document.getElementById("pic9").onmouseout = bbb;
 
 function aaa() {
+    for (let i = 1; i <= 9; i++) {
 
-    if (this.id == "pic1") { this.src = "./img/ice01-1.jpg" }
-    if (this.id == "pic2") { this.src = "./img/ice02-1.jpg" }
-    if (this.id == "pic3") { this.src = "./img/ice03-1.jpg" }
-    if (this.id == "pic4") { this.src = "./img/ice04-1.jpg" }
-    if (this.id == "pic5") { this.src = "./img/ice05-1.jpg" }
-    if (this.id == "pic6") { this.src = "./img/ice06-1.jpg" }
-    if (this.id == "pic7") { this.src = "./img/ice07-1.jpg" }
-    if (this.id == "pic8") { this.src = "./img/ice08-1.jpg" }
-    if (this.id == "pic9") { this.src = "./img/ice09-1.jpg" }
-
+        if (this.id == "pic" + i) { this.src = "./img/ice0" + i + "-1.jpg" }
+        // if (this.id == "pic2") { this.src = "./img/ice02-1.jpg" }
+        // if (this.id == "pic3") { this.src = "./img/ice03-1.jpg" }
+        // if (this.id == "pic4") { this.src = "./img/ice04-1.jpg" }
+        // if (this.id == "pic5") { this.src = "./img/ice05-1.jpg" }
+        // if (this.id == "pic6") { this.src = "./img/ice06-1.jpg" }
+        // if (this.id == "pic7") { this.src = "./img/ice07-1.jpg" }
+        // if (this.id == "pic8") { this.src = "./img/ice08-1.jpg" }
+        // if (this.id == "pic9") { this.src = "./img/ice09-1.jpg" }
+    }
 }
 
 
 function bbb() {
+    for (let i = 1; i <= 9; i++) {
+        if (this.id == "pic" + i) { this.src = "./img/ice0" + i + ".jpg" }
+        // if (this.id == "pic2") { this.src = "./img/ice02.jpg" }
+        // if (this.id == "pic3") { this.src = "./img/ice03.jpg" }
+        // if (this.id == "pic4") { this.src = "./img/ice04.jpg" }
+        // if (this.id == "pic5") { this.src = "./img/ice05.jpg" }
+        // if (this.id == "pic6") { this.src = "./img/ice06.jpg" }
+        // if (this.id == "pic7") { this.src = "./img/ice07.jpg" }
+        // if (this.id == "pic8") { this.src = "./img/ice08.jpg" }
+        // if (this.id == "pic9") { this.src = "./img/ice09.jpg" }
 
-    if (this.id == "pic1") { this.src = "./img/ice01.jpg" }
-    if (this.id == "pic2") { this.src = "./img/ice02.jpg" }
-    if (this.id == "pic3") { this.src = "./img/ice03.jpg" }
-    if (this.id == "pic4") { this.src = "./img/ice04.jpg" }
-    if (this.id == "pic5") { this.src = "./img/ice05.jpg" }
-    if (this.id == "pic6") { this.src = "./img/ice06.jpg" }
-    if (this.id == "pic7") { this.src = "./img/ice07.jpg" }
-    if (this.id == "pic8") { this.src = "./img/ice08.jpg" }
-    if (this.id == "pic9") { this.src = "./img/ice09.jpg" }
-
+    }
 }
-
 
 
