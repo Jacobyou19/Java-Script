dbae6ec0bc7e5958d9f0dba21cfa91bd6fc3bfcf
Jacobyou19 <jacobyou19@gmail.com>
2024-03-19 10:52:07 +0800

    am

--- 上課資料/郭嘉雯/09-滑鼠事件-event/09-4-手機隱藏式選單的冒泡事件/my.js
+++ 上課資料/郭嘉雯/09-滑鼠事件-event/09-4-手機隱藏式選單的冒泡事件/my.js
@@ -1,6 +1,32 @@
+document.getElementById("icon").onclick = show;
+
+document.body.onclick = hide;
+
+function show(e) {
+    
+    document.getElementById("menu").style.display = "block";
+    e.stopPropagation = ();
+}
+
+function hide() {
+    
+    document.getElementById("menu").style.display = "none";
+
+}
+    
+document.getElementById("aa").onclick = aa;
+document.getElementById("bb").onclick = bb;
+
+function aa(e) {
+
+    e.stopPropagation();
+    
+
+}
+
+function bb(e) {
+
+    e.stopPropagation();
 
 
-
-
-
-
+}
